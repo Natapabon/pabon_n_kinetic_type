2c9d713c8e54167c149a47a288d9ac91d6cce60d
Nata Pabon <ta@MacBook-Air-de-Ta.local>
2021-08-08 04:38:15 -0400

    fixing the type screen and the animation

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
       playButton = document.querySelector(".play_anim");
 
   function playAudio() {
+    document.querySelectorAll("g").forEach(g => g.classList.add("text-anim"));
+    document.querySelector("#typeLayout").classList.add("containerTurn")
     document.querySelector(".oogway").classList.add("characterAnimation");
     audioEl.play();
   }
